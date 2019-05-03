09daccf066aaf24ff20bf3eeed6c3e188fbb78db
Chris Canipe <chriscanipe@MacBook-Pro.local>
2019-05-03 10:49:30 -0400

    updated some structure stuff for demo

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
 //Global functions are declared outside of functions.
 //Global means we can access them from anywhere in our script.
 //These are all variables we have named ourselves
-let margin;
-let width;
-let height;
-let xScale;
-let yScale;
-let svg;
-let plot;
-let xAxis;
-let yAxis;
-let theData;
+
+let chart = {};
 
 
 
@@ -31,8 +23,8 @@ init();
 function init(data) {
 
     //This is called a ternary operator. It's a shorthand if/else statement.
-    //If `data` exists, theData is equal to `data`. If it doesnt' exist, define it as null.
-    theData = data ? data : null;
+    //If `data` exists, chart.data is equal to `data`. If it doesnt' exist, define it as null.
+    chart.data = data ? data : null;
 
     //Append our elements to the page. This only happens on load.
     appendElements();
@@ -40,6 +32,10 @@ function init(data) {
     //Update positions and styles for everything on the page
     //whenever we update the page (on re-size, for example).
     update();
+
+    d3.select(window).on("resize", d => {
+        update();
+    });
 
 }
 
@@ -61,18 +57,18 @@ function setDimensions() {
     //Arrays can contain objects [{foo: bar, color: "green"}, {hat: brown, "dog" : "fido"}]
 
     //This is an object
-    margin = {
+    chart.margin = {
         top: 30,
         right: 30,
         bottom: 40,
         left: 100
     };
 
-    let chartWidth = document.querySelector(".chart").offsetWidth;
-    let chartHeight = document.querySelector(".chart").offsetHeight;
+    let targetWidth = document.querySelector(".chart").offsetWidth;
+    let targetHeight= document.querySelector(".chart").offsetHeight;
 
-    width = chartWidth - margin.left - margin.right;
-    height = chartHeight - margin.top - margin.bottom;
+    chart.width = targetWidth - chart.margin.left - chart.margin.right;
+    chart.height = targetHeight - chart.margin.top - chart.margin.bottom;
 }
 
 
@@ -80,12 +76,12 @@ function setScales() {
 
     //These d3.scaleLinear() elements are functions that exist in D3
 
-    xScale = d3.scaleLinear() //This is a linear scale
-        .rangeRound([0, width]) //Its "range" is the width of `this.plot`
+    chart.xScale = d3.scaleLinear() //This is a linear scale
+        .rangeRound([0, chart.width]) //Its "range" is the width of `this.plot`
         .domain([0, 100]); //Its "domain" defaults to 0 to 100.
 
-    yScale = d3.scaleLinear()
-        .rangeRound([height, 0])
+    chart.yScale = d3.scaleLinear()
+        .rangeRound([chart.height, 0])
         .domain([0, 100]);
 
 
@@ -94,19 +90,21 @@ function setScales() {
 
 
 
+
+
 function appendElements() {
 
     //SVG is the container.
-    svg = d3.select(".chart").append("svg");
+    chart.svg = d3.select(".chart").append("svg");
 
     //The plot is where the charting action happens.
-    plot = svg.append("g").attr("class", "chart-g");
+    chart.plot = chart.svg.append("g").attr("class", "chart-g");
 
     //The xAxis and yAxis group tags will hold our xAxis elements (ticks, etc.)
-    xAxis = plot.append("g")
+    chart.xAxis = chart.plot.append("g")
         .classed("axis x-axis", true);
 
-    yAxis = plot.append("g")
+    chart.yAxis = chart.plot.append("g")
         .classed("axis y-axis", true);
 
 }
@@ -117,28 +115,26 @@ function appendElements() {
 function updateElements() {
 
     //The this.svg will be the FULL width and height of the parent container (this.element)
-    svg.attr("width", width + margin.left + margin.right);
-    svg.attr("height", height + margin.top + margin.bottom);
+    chart.svg.attr("width", chart.width + chart.margin.left + chart.margin.right);
+    chart.svg.attr("height", chart.height + chart.margin.top + chart.margin.bottom);
 
     //this.plot is offset from the top and left of the this.svg
-    plot.attr("transform", `translate(${margin.left},${margin.top})`);
+    chart.plot.attr("transform", `translate(${chart.margin.left},${chart.margin.top})`);
 
     //This is where the axis elements get drawn. The "transform" property positions them
     //And the the .call() method draws the axis within that tag.
     //Most of the logic is behind the scenes
-    xAxis.attr("transform", "translate(0," + (height + 20) + ")")
+    chart.xAxis.attr("transform", "translate(0," + (chart.height + 20) + ")")
         .call(
-            d3.axisBottom(xScale)
-            .tickSize(-height - 20)
+            d3.axisBottom(chart.xScale)
+            .tickSize(-chart.height - 20)
         );
 
-    yAxis.attr("transform", `translate(-20,0)`)
+    chart.yAxis.attr("transform", `translate(-20,0)`)
         .call(
-            d3.axisLeft(yScale)
-            .tickSize(-width - 20)
+            d3.axisLeft(chart.yScale)
+            .tickSize(-chart.width - 20)
         );
-
-
 
 }
 
